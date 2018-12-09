a59814ddea5c5b42966c20b76fa214441d83c22e
kndlt <kndlt22@gmail.com>
2018-12-08 21:28:23 -0800

    Revert "non symlink"
    This reverts commit 613ad59ba75f755f024d586cf03d9868db3be9d4.

--- webpack.common.js
+++ webpack.common.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   output: {
     filename: 'app.bundle.js',
-    path: path.resolve(__dirname, 'docs')
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new HtmlWebpackPlugin({template: './src/index.html'})
