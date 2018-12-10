b420bb209ed3fa012adaa585170cebed085c13b8
kndlt <kndlt22@gmail.com>
2018-12-10 00:54:56 -0800

    camera control

--- src/Components/VoxelShader/VoxelShader.tsx
+++ src/Components/VoxelShader/VoxelShader.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { Shaders, Node, GLSL } from "gl-react";
-import { Vector3 } from "three";
 
 interface VoxelShaderProps {
-  eye: Readonly<THREE.Vector3>;
-  matrixWorldInverse: Readonly<THREE.Matrix4>;
-  projectionMatrixInverse: Readonly<THREE.Matrix4>;
+  eye: number[];
+  matrixWorldInverse: Float32Array;
+  projectionMatrixInverse: Float32Array;
 }
 
 const shaders = Shaders.create({
-  helloBlue: {
+  vt01: {
     frag: GLSL`
       precision highp float;
       varying vec2 uv;
-      uniform float blue;
       uniform mat4 matrixWorldInverse;
       uniform mat4 projectionMatrixInverse;
       uniform vec3 eye;
@@ -21,24 +19,26 @@ const shaders = Shaders.create({
         // TODO Optimize by pre-computing matrices or initial rays itself
         vec4 q = matrixWorldInverse * projectionMatrixInverse * vec4(uv - 0.5, 0.0, 1.0);
         vec3 initialRay = normalize(q.xyz / q.w - eye);
-        gl_FragColor = projectionMatrixInverse * vec4(initialRay * 255.0, 1.0);
+        gl_FragColor = projectionMatrixInverse * vec4(initialRay, 1.0);
       }
     `
   }
 });
 
 export default class VoxelShader extends React.Component<VoxelShaderProps> {
+
   render() {
+
     const { eye, matrixWorldInverse, projectionMatrixInverse } = this.props;
     // console.log(projectionMatrixInverse.multiplyVector4(new Vector4(1,1,0,1)));
     return (
-      <Node shader={shaders.helloBlue} uniforms={{
-        blue: 0,
-        eye: eye.toArray(),
-        matrixWorldInverse: matrixWorldInverse.elements,
-        projectionMatrixInverse: projectionMatrixInverse.elements
+      <Node shader={shaders.vt01} uniforms={{
+        eye,
+        matrixWorldInverse,
+        projectionMatrixInverse
       }} />
     );
 
   }
+
 }
--- src/Components/VoxelViewer/VoxelViewer.tsx
+++ src/Components/VoxelViewer/VoxelViewer.tsx
@@ -1,27 +1,75 @@
 import React from "react";
-import * as THREE from "three";
+import * as THREE from 'three';
+import { Scene, PerspectiveCamera, Vector3, Matrix4 } from "three";
 import { Surface } from "gl-react-dom";
 import VoxelShader from '../VoxelShader/VoxelShader';
 
-export default class VoxelViewer extends React.Component {
-  scene: THREE.Scene;
-  camera: THREE.PerspectiveCamera;
+const OrbitControls = require('three-orbit-controls')(THREE);
 
-  constructor(props: Readonly<{}>) {
+interface OrbitControls extends THREE.OrbitControls {}
+
+interface VoxelViewerProps { }
+
+interface VoxelViewerStateProps {
+  eye: number[],
+  matrixWorldInverse: Float32Array,
+  projectionMatrixInverse: Float32Array
+}
+
+export default class VoxelViewer extends React.Component<VoxelViewerProps, VoxelViewerStateProps> {
+  scene: Scene;
+  camera: PerspectiveCamera;
+  orbitControls?: OrbitControls;
+
+  constructor(props: VoxelViewerProps) {
 
     super(props);
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(75, 1, 0.01, 1000);
-    this.camera.position.z = 5;
+    this.scene = new Scene();
+    this.camera = new PerspectiveCamera(75, 1, 0.01, 1000);
+    this.camera.position.set(0, 0, 5);
+    this.camera.lookAt(new Vector3(0, 0, 0));
+    this.state = {
+      eye: this.camera.position.toArray(),
+      matrixWorldInverse: this.camera.matrixWorldInverse.elements,
+      //@ts-ignore
+      projectionMatrixInverse: this.camera.projectionMatrixInverse.elements
+    };
+  }
+
+  didOrbit = (event: Event) => {
+    this.cameraDidUpdate();
+  }
+
+  cameraDidUpdate() {
+    this.setState({
+      eye: this.camera.position.toArray(),
+      matrixWorldInverse: this.camera.matrixWorldInverse.elements,
+      //@ts-ignore
+      projectionMatrixInverse: this.camera.projectionMatrixInverse.elements
+    });
+  }
+
+  componentDidMount() {
+    const orbitControls = this.orbitControls = new OrbitControls( this.camera );
+    orbitControls.addEventListener('change', this.didOrbit);
+  }
+
+  componentWillUnmount() {
+
+    if (this.orbitControls) {
+      this.orbitControls.removeEventListener('change', this.didOrbit);
+      this.orbitControls.dispose();
+      delete this.orbitControls;
+    }
 
   }
 
   render() {
 
-    const eye: THREE.Vector3 = this.camera.position;
-    const matrixWorldInverse: THREE.Matrix4 = this.camera.matrixWorldInverse;
-    // @ts-ignore
-    const projectionMatrixInverse: THREE.Matrix4 = this.camera.projectionMatrixInverse;
+    // const eye: Vector3 = this.camera.position;
+    // const matrixWorldInverse: Matrix4 = this.camera.matrixWorldInverse;
+    // // @ts-ignore
+    // const projectionMatrixInverse: Matrix4 = this.camera.projectionMatrixInverse;
 
     return (
       // Matrix4 invertedModelViewProjectionMatrix =
@@ -30,9 +78,9 @@ export default class VoxelViewer extends React.Component {
       // @ts-ignore
       <Surface width={300} height={300}>
         <VoxelShader
-          eye={eye}
-          matrixWorldInverse={matrixWorldInverse}
-          projectionMatrixInverse={projectionMatrixInverse}
+          eye={this.state.eye}
+          matrixWorldInverse={this.state.matrixWorldInverse}
+          projectionMatrixInverse={this.state.projectionMatrixInverse}
         />
       </Surface>
     );
