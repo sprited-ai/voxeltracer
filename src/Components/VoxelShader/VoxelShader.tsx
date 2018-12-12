f22b55b8be7ba2bd93c3ce975c41dcbcce6cf785
kndlt <kndlt22@gmail.com>
2018-12-12 01:26:53 -0800

    random

--- src/Components/VoxelShader/VoxelShader.tsx
+++ src/Components/VoxelShader/VoxelShader.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Shaders, Node, GLSL } from "gl-react";
+import { randomFunction } from "../../Shaders/randomFunction";
 
 interface VoxelShaderProps {
   eye: number[];
@@ -15,11 +16,15 @@ const shaders = Shaders.create({
       uniform mat4 matrixWorldInverse;
       uniform mat4 projectionMatrixInverse;
       uniform vec3 eye;
+      ${randomFunction}
       void main() {
-        // TODO Optimize by pre-computing matrices or initial rays itself
-        vec4 q = matrixWorldInverse * projectionMatrixInverse * vec4(uv - 0.5, 0.0, 1.0);
-        vec3 initialRay = normalize(q.xyz / q.w - eye);
-        gl_FragColor = projectionMatrixInverse * vec4(initialRay, 1.0);
+        // // TODO Optimize by pre-computing matrices or initial rays itself
+        // vec4 q = matrixWorldInverse * projectionMatrixInverse * vec4((uv - 0.5) * 2.0, 0.0, 1.0);
+        // vec3 initialRay = normalize(q.xyz / q.w - eye);
+        // gl_FragColor = projectionMatrixInverse * vec4(initialRay * 255.0, 1.0);
+
+        float rand = random(vec3(12.9898, 78.233, 151.7182), uv.x);
+        gl_FragColor = vec4(rand, rand, rand, 1.0);
       }
     `
   }
