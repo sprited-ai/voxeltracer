a78199180adb82177a92762d5276f41962df8ab4
kndlt <kndlt22@gmail.com>
2019-01-24 00:19:49 -0800

    gh-pages

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -403,6 +403,13 @@ module.exports = {
               'sass-loader'
             ),
           },
+          {
+            test: /\.(glsl|vs|fs)$/,
+            use: [
+              'raw-loader',
+              'glslify-loader'
+            ]
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
