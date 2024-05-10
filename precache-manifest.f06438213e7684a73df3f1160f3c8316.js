21001c570ac7787915e2c8a5d23af7aa5fe1bb86
Jin Hyuk Cho <2665740+kndlt@users.noreply.github.com>
2024-05-09 22:42:15 -0700

    Updates

--- precache-manifest.f06438213e7684a73df3f1160f3c8316.js
+++ precache-manifest.f06438213e7684a73df3f1160f3c8316.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "b5a3189ea251e7531c79",
-    "url": "/voxelviewer/static/css/main.0a245e93.chunk.css"
+    "revision": "f5db80537b2b15ea4be5",
+    "url": "/voxelviewer/static/css/main.3ac4a0e9.chunk.css"
   },
   {
-    "revision": "b5a3189ea251e7531c79",
-    "url": "/voxelviewer/static/js/main.b5a3189e.chunk.js"
+    "revision": "f5db80537b2b15ea4be5",
+    "url": "/voxelviewer/static/js/main.f5db8053.chunk.js"
   },
   {
     "revision": "d8c3993a4a2d8a19641e",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/voxelviewer/static/js/runtime~main.a0ffb064.js"
   },
   {
-    "revision": "7fe8a511db3e386ca8e256a589717fd2",
+    "revision": "3b11fc80037c04d187d3d388738d8b35",
     "url": "/voxelviewer/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/voxelviewer/precache-manifest.2d6af1d85f7c43206b74fcbd7ee1980b.js"
+  "/voxelviewer/precache-manifest.f06438213e7684a73df3f1160f3c8316.js"
 );
 
 workbox.clientsClaim();
